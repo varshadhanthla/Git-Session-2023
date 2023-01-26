b4ebedc1ee5836e280b9c6fdac91874e5bea49ad
Prince Singh <31801256+TsarPrince@users.noreply.github.com>
2023-01-26 16:21:23 +0530

    Upload your files here

---
+++ Place file here/sample.js
@@ -0,0 +1,11 @@
+console.log("Welcome to the ultimate experience!")
+console.log("Get ready to be amazed as we take you on a journey through the unknown, uncovering hidden treasures and uncovering secrets that have been long forgottenBuckle up and hold on tight, this is going to be one wild ride!")
+
+console.log(`
+    ░██████╗░██╗████████╗██╗░░██╗██╗░░░██╗██████╗░
+    ██╔════╝░██║╚══██╔══╝██║░░██║██║░░░██║██╔══██╗
+    ██║░░██╗░██║░░░██║░░░███████║██║░░░██║██████╦╝
+    ██║░░╚██╗██║░░░██║░░░██╔══██║██║░░░██║██╔══██╗
+    ╚██████╔╝██║░░░██║░░░██║░░██║╚██████╔╝██████╦╝
+    ░╚═════╝░╚═╝░░░╚═╝░░░╚═╝░░╚═╝░╚═════╝░╚═════╝░
+`)
